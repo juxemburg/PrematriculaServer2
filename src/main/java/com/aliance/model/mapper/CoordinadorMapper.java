45678ab1b1d67dfc90fa85ae190c0a55e9cc8f83
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2017-11-19 21:58:55 -0500

    se agregó soporte para datasources de oracle

--- src/main/java/com/aliance/model/mapper/CoordinadorMapper.java
+++ src/main/java/com/aliance/model/mapper/CoordinadorMapper.java
@@ -6,14 +6,13 @@ import prematriculaClient.Docente;
 public class CoordinadorMapper {
 
     public static CoordinadorModel getCoordinador(Docente docente) {
-         try
-         {
-             return new CoordinadorModel("",
-                     docente.getPrimerNombre() +" "+docente.getSegundoNombre(),
-                     docente.getPrimerApellido() +" "+docente.getSegundoApellido(),
-                     docente.getOidProgramaCoordinado(), docente.getIdentificacion());
-         }catch (Exception e) {
-             return null;
-         }
+        try {
+            return new CoordinadorModel("",
+                    docente.getPrimerNombre() + " " + docente.getSegundoNombre(),
+                    docente.getPrimerApellido() + " " + docente.getSegundoApellido(),
+                    docente.getOidProgramaCoordinado(), docente.getIdentificacion());
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
